0158cb0728a6fbae731aa34dcb4f1578e64df31f
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2017-09-14 14:54:58 +0300

    fix download

--- download.py
+++ download.py
@@ -11,10 +11,10 @@ cookie = {
     'session' : 'eyJjc3JmIjoiYlZINGFPb0kxNXlnN3V1cXFveUlYaCJ9|1505115716|eff56d3da7623bd2aef4f8d43247ef75bcd75f2d',
     'ajs_anonymous_id' : '%22e709dd72-97e4-444f-b685-73913b836bb8%22',
     'csrf' : 'bVH4aOoI15yg7uuqqoyIXh',
-    'auth' : '"eyJfdXNlciI6WzU2Nzc1ODY3MDUyODUxMjAsMSwiZzNlN05sU2U1bmJWR2VBOGV1YkY0byIsMTUwNTI5NTY1MSwxNTA1Mzc4NjU5LCJWaXRhbGlpIFZydWJsZXZza3lpIiwwXX0\075|1505379129|06c4dbbd97fcda18736dc32a9851bebd75299725"',
+    'auth' : '"eyJfdXNlciI6WzU2Nzc1ODY3MDUyODUxMjAsMSwiZHZQV0JLQWtydlRsT0hhaGRTNUdaSSIsMTUwNTM4OTg5MSwxNTA1Mzg5ODkxLCJWaXRhbGlpIFZydWJsZXZza3lpIiwwXX0\075|1505389892|a7f849e25b4c11e40dbb2eb028f98296b5c4360e"',
     '_ga' : 'GA1.2.923972133.1505115718',
     '_gid' : 'GA1.2.857001817.1505295653',
-    'v' : '31',
+    'v' : '34',
     'ajs_user_id' : 'null',
     'ajs_group_id' : 'null',
     '_gat' : '1'

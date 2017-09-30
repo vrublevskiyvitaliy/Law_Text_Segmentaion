33c4f04b95bb80a1b411c005c74751ca49be3270
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2017-09-30 11:50:00 +0300

    add docx_to_txt.py

--- docx_to_txt.py
+++ docx_to_txt.py
@@ -1,5 +1,19 @@
 import docx2txt
+from os import listdir
+from os.path import isfile, join
 
-a = docx2txt.process('documents/file0.docx').encode('utf-8')
-f = open('file0.txt', 'w')
-f.write(a)
+documents_path = 'documents/'
+txt_path = 'txt/'
+
+
+def convert_docx_to_txt():
+    files_docx = [f for f in listdir(documents_path) if isfile(join(documents_path, f)) and f[-4:] == 'docx']
+
+    for file in files_docx:
+        docx_path = documents_path + file
+        path = txt_path + file[:-4] + 'txt'
+        a = docx2txt.process(docx_path).encode('utf-8')
+        f = open(path, 'w')
+        f.write(a)
+
+convert_docx_to_txt()

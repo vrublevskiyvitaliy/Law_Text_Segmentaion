1bcc4d70b95641c9b17eda61f2c269d38bf0450e
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2017-10-13 21:34:22 +0300

    add bad_docx.json use it in download_many and filter

--- download_many.py
+++ download_many.py
@@ -11,23 +11,42 @@ def random_wait():
     time.sleep(random.randint(3, 10))
 
 
+def get_bad_docx():
+    with open('bad_docx.json') as data_file:
+        data = json.load(data_file)
+    return data
+
 def download_docx_contracts(contracts_ids, contracts_num):
+    # cookie = {
+    #     '__cfduid': 'd4a23cb5a2d676396be7fd88a0b82b78f1505115716',
+    #     'session': 'eyJjc3JmIjoiYlZINGFPb0kxNXlnN3V1cXFveUlYaCJ9|1505115716|eff56d3da7623bd2aef4f8d43247ef75bcd75f2d',
+    #     'ajs_anonymous_id': '%22e709dd72-97e4-444f-b685-73913b836bb8%22',
+    #     'csrf': 'bVH4aOoI15yg7uuqqoyIXh',
+    #     'auth': 'eyJfdXNlciI6WzU2Nzc1ODY3MDUyODUxMjAsMSwiNXUyelFiZzE4OE1oeGRscXVzQkZGSCIsMTUwNzkxNTIyMCwxNTA3OTE1MjIwLCJWaXRhbGlpIFZydWJsZXZza3lpIl19|1507915225|c2bc4016f50572f1e59d467cb4304b9af3b6c6ba',
+    #     '_ga': 'GA1.2.923972133.1505115718',
+    #     '_gid': 'GA1.2.1020241738.1506851128',
+    #     'v': '43',
+    #     'ajs_user_id': 'null',
+    #     'ajs_group_id': 'null',
+    #     '_gat': '1'
+    # }
+
     cookie = {
-        '__cfduid': 'd4a23cb5a2d676396be7fd88a0b82b78f1505115716',
-        'session': 'eyJjc3JmIjoiYlZINGFPb0kxNXlnN3V1cXFveUlYaCJ9|1505115716|eff56d3da7623bd2aef4f8d43247ef75bcd75f2d',
-        'ajs_anonymous_id': '%22e709dd72-97e4-444f-b685-73913b836bb8%22',
-        'csrf': 'bVH4aOoI15yg7uuqqoyIXh',
-        'auth': 'eyJfdXNlciI6WzU2Nzc1ODY3MDUyODUxMjAsMSwiWGkyNFBJaXV0MXVSOGYzcXNHbGdKWSIsMTUwNjg1MTM5OSwxNTA2ODUxMzk5LCJWaXRhbGlpIFZydWJsZXZza3lpIl19|1506851399|8b5ee1a015bfe942582b67945a6cbeb068ad6201',
-        '_ga': 'GA1.2.923972133.1505115718',
-        '_gid': 'GA1.2.1020241738.1506851128',
-        'v': '43',
-        'ajs_user_id': 'null',
-        'ajs_group_id': 'null',
-        '_gat': '1'
+        # '__cfduid': 'd4a23cb5a2d676396be7fd88a0b82b78f1505115716',
+        # 'session': 'eyJjc3JmIjoiYlZINGFPb0kxNXlnN3V1cXFveUlYaCJ9|1505115716|eff56d3da7623bd2aef4f8d43247ef75bcd75f2d',
+        # 'ajs_anonymous_id': '%22e709dd72-97e4-444f-b685-73913b836bb8%22',
+        # 'csrf': 'bVH4aOoI15yg7uuqqoyIXh',
+        'auth': 'eyJfdXNlciI6WzU3NjE5MzUwODE0NzIwMDAsMSwickxIOWhDZ2w2V0FnOWtBSWI3Q2F4eSIsMTUwNzkxNTY4NywxNTA3OTE1Njg3LG51bGxdfQ\075\075|1507915706|2e5de8e3d19c5b6c8462a99cab68eb77f4f8c5aa',
     }
 
+
+    bad_docx = get_bad_docx()
     ind = 0
     for contract_id in contracts_ids:
+        if contract_id + '.docx' in bad_docx:
+            print('Found bad docx')
+            continue
+        print (str(ind))
         if (ind == contracts_num):
             break
         url = 'https://www.lawinsider.com/contracts/' + contract_id + '.docx'
@@ -62,4 +81,4 @@ def get_contracts_ids():
 
 
 contracts_ids = get_contracts_ids()
-download_docx_contracts(contracts_ids, 700)
+download_docx_contracts(contracts_ids[1000:], 5)
--- filter_docx.py
+++ filter_docx.py
@@ -2,19 +2,38 @@ import docx2txt
 import os
 from os import listdir
 from os.path import isfile, join
-
+import json
 documents_path = 'documents/'
+
+
+def add_to_bad_docx(list):
+    with open('bad_docx.json') as data_file:
+        data = json.load(data_file)
+
+    for item in list:
+        data.append(item)
+
+    data = list(set(data))
+
+    with open('bad_docx.json', 'w') as outfile:
+        json.dump(data, outfile)
 
 
 def filter_docx():
     files_docx = [f for f in listdir(documents_path) if isfile(join(documents_path, f)) and f[-4:] == 'docx']
 
+    filtered = 0
+    list = []
     for file in files_docx:
         docx_path = documents_path + file
         try:
             a = docx2txt.process(docx_path).encode('utf-8')
         except Exception as e:
             print(file)
+            list.append(file)
             os.remove(docx_path)
+            filtered += 1
+    print(str(filtered))
+    add_to_bad_docx(list)
 
 filter_docx()
